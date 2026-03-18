6ed0b9af0ad8eb80142155da77250d1ef5c2568f
Arjun Thakre <arjunthakre2022@gmail.com>
2026-03-18 20:53:39 +0530

    implement Api and Rate Limiter

---
+++ api-gateway/src/main/java/com/paypal/api_gateway/utils/JwtUtils.java
@@ -0,0 +1,24 @@
+package com.paypal.api_gateway.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+
+public class JwtUtils {
+    private static final String SECRET =  "secret123secret123secret123secret123secret123secret123secret123";
+
+
+    private static Key getSigniningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public static Claims validateToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigniningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}

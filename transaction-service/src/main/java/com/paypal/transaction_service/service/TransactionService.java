a283e1ea00116775b10ad440d13f7590c8441251
Arjun Thakre <arjunthakre2022@gmail.com>
2026-03-25 20:20:39 +0530

    update

--- transaction-service/src/main/java/com/paypal/transaction_service/service/TransactionService.java
+++ transaction-service/src/main/java/com/paypal/transaction_service/service/TransactionService.java
@@ -7,5 +7,6 @@ import java.util.List;
 public interface TransactionService {
 
     Transaction createTransaction(Transaction transaction);
-    List<Transaction> getAllTransactions();
+    public Transaction getTransactionById(Long id);
+    public List<Transaction> getTransactionsByUser(Long userId);
 }
